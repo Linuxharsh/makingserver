9c928feacc58a99bae1d5d6e546b17bc381c46ae
HARSH PANDEY <145363773+Linuxharsh@users.noreply.github.com>
2025-05-17 11:43:21 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const http = require('http');
 
+//const server = require('./handling');
 
+const express = require('express');
 
-const harsh = http.createServer((req , rep) => {
-  console.log(req.url,req.method,req.headers);
+const app = express();
+
+app.get("/", (req, res, next) => {
+  console.log("This is home page", req.url, req.method);
+  res.send("<h1>Harsh pandey</h1>");
+
 });
-const PORT=3000;
-harsh.listen(PORT,() => {
-console.log('server running on address http://localhost:${PORT}');
+app.get("/contact-us", (req, res, next) => {
+  console.log("This is second page", req.url, req.method);
+  res.send(`
+    <h1>Submit your details</h1>
+    <form action="/contact-us" method="POST">
+      <input type="text" name="name" placeholder="Enter your name"/>
+      <input type="email" name="email" placeholder="Enter your email"/>
+      <input type="Submit"/>
+    </form>
+  `);
+});
+app.post("/contact-us",(req, res , next)=> {
+  console.log("thank you ",req.url,req.method);
+  res.send("<h1>We will contact us early </h1>");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
 });
